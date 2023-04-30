b4d175a802d027d7dc89a0d38a7af91133d79fb6
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2023-04-30 16:38:09 +0100

    Fixed some formatting

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
 extern crate vulkano;
-extern crate winit;
 extern crate vulkano_win;
+extern crate winit;
 
 mod halvar {
     use vulkano_win::VkSurfaceBuild;
     use winit::{
+        dpi::LogicalSize,
         event::{Event, WindowEvent},
         event_loop::EventLoop,
-        window::{WindowBuilder, Window},
-        dpi::LogicalSize
+        window::{Window, WindowBuilder},
     };
 
     use std::sync::Arc;
-    use vulkano::{instance::{
-        Instance,
-        InstanceCreateInfo
-    }, VulkanLibrary, swapchain::Surface};
+    use vulkano::{
+        instance::{Instance, InstanceCreateInfo},
+        swapchain::Surface,
+        VulkanLibrary,
+    };
 
     pub struct Application {
         instance: Arc<Instance>,
         surface: Arc<Surface>,
-        event_loop: EventLoop<()>
+        event_loop: EventLoop<()>,
     }
 
     impl Application {
-        pub fn new()->Self {
+        pub fn new() -> Self {
             let instance = Self::create_instance();
             let event_loop = EventLoop::new();
             let surface = WindowBuilder::new()
@@ -32,7 +33,11 @@ mod halvar {
                 .with_inner_size(LogicalSize::new(800, 600))
                 .build_vk_surface(&event_loop, instance.clone())
                 .unwrap();
-            Application { instance, surface, event_loop }
+            Application {
+                instance,
+                surface,
+                event_loop,
+            }
         }
 
         pub fn create_instance() -> Arc<Instance> {
@@ -42,42 +47,39 @@ mod halvar {
 
             let instance = Instance::new(
                 library,
-                InstanceCreateInfo { 
-                    enabled_extensions: required_extensions, 
+                InstanceCreateInfo {
+                    enabled_extensions: required_extensions,
                     engine_name: Some("Halvar".into()),
                     enumerate_portability: true,
                     ..Default::default()
-                    },
-            ).unwrap();
+                },
+            )
+            .unwrap();
 
             return instance;
         }
-    
+
         pub fn run(self) {
-    
-            self.event_loop.run(move |event, _, control_flow| {
-    
-                    match event {
-                        Event::WindowEvent {
-                            event: WindowEvent::CloseRequested,
-                            ..
-                        } => control_flow.set_exit(),
-                        Event::RedrawEventsCleared => {
-                            let window = self.surface.object()
-                                .unwrap()
-                                .downcast_ref::<Window>().unwrap();
-                            let dimensions = window.inner_size();
-                            if dimensions.width == 0 || dimensions.height == 0 {
-                                return;
-                            }
-
-
+            self.event_loop
+                .run(move |event, _, control_flow| match event {
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => control_flow.set_exit(),
+                    Event::RedrawEventsCleared => {
+                        let window = self
+                            .surface
+                            .object()
+                            .unwrap()
+                            .downcast_ref::<Window>()
+                            .unwrap();
+                        let dimensions = window.inner_size();
+                        if dimensions.width == 0 || dimensions.height == 0 {
+                            return;
                         }
-                        _=> (),
                     }
-                    
+                    _ => (),
                 });
-            
         }
     }
 }
