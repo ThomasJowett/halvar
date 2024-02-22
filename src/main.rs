05f81c4807071edb45754ee93fa6678feb94d915
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2024-02-22 14:59:12 +0000

    switched to using wgpu as the renderer

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,6 @@
-extern crate vulkano;
-extern crate vulkano_win;
-extern crate winit;
 
-mod application;
-mod vertex;
-mod shaders;
+use halvar::halvar::run;
 
 fn main() {
-    use vertex::halvar::StandardVertex;
-
-    let app = application::halvar::Application::new();
-
-    let _vertices = [
-        StandardVertex {
-            position: [-0.5, -0.25],
-        },
-        StandardVertex {
-            position: [0.0, 0.5],
-        },
-        StandardVertex {
-            position: [0.25, -0.1],
-        },
-    ];
-    app.run();
+    run();
 }
